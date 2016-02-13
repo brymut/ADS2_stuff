709e4608a8ce7e5b2c25eadf059f9f853c66f1a7
brymut <mutaiwork@gmail.com>
2016-02-13 13:53:38 +0000

    complete with comments

--- ex2/ArrayQueue.java
+++ ex2/ArrayQueue.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class ArrayQueue<E> {
 
+    //instance variables
     public static final int CAPACITY = 10;  // default queue capacity
     private E[] Q;                          // E array used to implement the queue  
     private int n;                          // actual capacity of queue
@@ -10,24 +11,30 @@ public class ArrayQueue<E> {
     private int rear;                       // rear of the queue
     private int size;                       // size of the queue
     
-	
-    public ArrayQueue(){this(CAPACITY);}
+	// constructors
+    public ArrayQueue(){this(CAPACITY);}   // constructs queue with defualt capacity
 
     @SuppressWarnings("unchecked")
-    public ArrayQueue(int capacity){
+    public ArrayQueue(int capacity){     // constructs queue with specified capacity
 	n = capacity;
-	Q = (E[]) new Object [n]; 
+	Q = (E[]) new Object [n];            // safe cast.
 	front = rear = size = 0;
     }
     //
     // NOTE: java does not allow creation of array with parametrised type!
     //
 	
+
+    //methods
+    //returns the size of the queue
     public int size(){return size;}
-    
+    //checks if the queue is empty
     public boolean isEmpty(){return (size==0);}
     
 
+
+    // returns first element of the queue, but doesn't remove.
+    // if ArrayQueue is empty returns null
     public E front() throws ArrayQueueException {
 
         if (isEmpty()) return null;
@@ -37,13 +44,16 @@ public class ArrayQueue<E> {
 
     }
     
-	
+	// Inserts element at the rear of the queue
     public void enqueue(E element) throws ArrayQueueException {
 
         if (size >= n)  throw new ArrayQueueException("ArrayQueue is full!");
         Q[rear] = element;
+        
 
-        rear = (rear+1)% n;
+        // use of modulo operator, ideal for treating array circulary.
+        // rear will resolve to 0 when the rear index value increments beyond n
+        rear = (rear+1)% n;  
 
         size++;
 
@@ -51,12 +61,16 @@ public class ArrayQueue<E> {
     }
     
 
-    
+    // Removes and returns the first element of the queue.
+    // returns null if the queue is empty
     public E dequeue() throws ArrayQueueException {
 
         if (isEmpty()) throw new ArrayQueueException("Unable to dequeue, queue is Empty");
 
         E firstElement = Q[front];
+
+        // use of modulo operator, ideal for treating array circulary.
+        // front will resolve to 0 when the front index value increments beyond n
 
         front=(front+1)%n;
         size--;
@@ -64,6 +78,8 @@ public class ArrayQueue<E> {
         return firstElement;}
     
 
+    // returns the string representation of the Queue, each element separated by a comma
+    // using StringBuilder
     public String toString(){
 
         StringBuilder output = new StringBuilder();
--- ex2/QueueSort.java
+++ ex2/QueueSort.java
@@ -5,22 +5,28 @@ import java.io.*;
 
 public class QueueSort<E extends Comparable<E>>  {
 
+    //instance variables
     private ArrayQueue<ArrayQueue<E>> Q;
     public static final int CAPACITY = 10;  // default queue capacity
-    private int n;    
+    private int n;                          // capacity of queue
     private boolean trace;
+
+
+
+
+    // constructors	
+    public QueueSort(){this(CAPACITY);}    //constructs queue with default capacity
 	
-    public QueueSort(){this(CAPACITY);}
-	
-    public QueueSort(int capacity){
+    public QueueSort(int capacity){        // constructs queue with specified capacity
 	n = capacity;
 	Q = new ArrayQueue<ArrayQueue<E>>(n);
     }
 
 
+    // methods
 
-
-
+    // Take two sorted queues and merge them to produce a third
+    // sorted queue
     private ArrayQueue<E> merge(ArrayQueue<E> q1,ArrayQueue<E> q2) throws ArrayQueueException {
 
         ArrayQueue<E> q3 = new ArrayQueue<E>(q1.size()+q2.size());
@@ -28,12 +34,17 @@ public class QueueSort<E extends Comparable<E>>  {
 
         while((!q1.isEmpty()) && (!q2.isEmpty())){
 
+        //If the first element of q1 is smaller than that of q2, it dequeues q1
+        //It dequeues element from q1 then enqueues element onto q3.  
             if(q1.front().compareTo(q2.front()) < 0) q3.enqueue(q1.dequeue());
+
+        //If the first element of q1 is smaller than that of q2, it dequeues q1
+        //It dequeues element from q1 then enqueues element onto q3.  
             else q3.enqueue(q2.dequeue());
 
 
         }
-
+        //if any elements are left over on q1, or q2.. add them on to q3
         if(!q1.isEmpty() ){
 
             for ( int i = 0; i < q1.size();i++) {
@@ -58,14 +69,21 @@ public class QueueSort<E extends Comparable<E>>  {
     }
     
     
-
+    // given a queue Q of queues
+    //  (1)   <a>if Q is of size 1 deliver the first queue in Q
+    //        if Q is of size 2 or more 
+    //        <b>  - get the first and second queues off Q
+    //        <b>  - merge these two queues to create a third queue
+    //        <b>  - add the third queue to the queue
+    //        - go back to (1)
+    //
     public void sort(){
 
 
-        while (Q.size() > 1) {
+        while (Q.size() > 1) {  //<a>
 
            
-           Q.enqueue(merge(Q.dequeue(),Q.dequeue()));
+           Q.enqueue(merge(Q.dequeue(),Q.dequeue())); //<b>
            
          }
 
@@ -73,32 +91,23 @@ public class QueueSort<E extends Comparable<E>>  {
 
 
     }
+    
+    
+    // <a> - create an ArrayQueue<E> that contains the element 
+    // <b> - enqueue it onto Q
     //
-    // IMPLEMENT ME
-    // given a queue Q of queues
-    // (1) if Q is of size 1 deliver the first queue in Q
-    // (2) if Q is of size 2 or more 
-    //     - get the first and second queues off Q
-    //     - merge these two queues to create a third queue
-    //     - add the third queue to the queue
-    //     - go back to (1)
-    //
-
     public void add(E element){
 
-        ArrayQueue<E> qNew = new ArrayQueue<E>();
+        ArrayQueue<E> qNew = new ArrayQueue<E>();   //<a>
 
-        qNew.enqueue(element);
+        qNew.enqueue(element);   //<a>
 
-        Q.enqueue(qNew);
+        Q.enqueue(qNew);       //<b>
 
     }
-    //
-    // IMPLEMENT ME
-    // create an ArrayQueue<E> that contains the element
-    // enqueue it onto Q
-    //
     
+   
+    // string representation of Queue
     public String toString(){return Q.toString();}
 
     public void trace(){trace = !trace;}
